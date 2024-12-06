31ba1dd252bbcf87db63ff4b0fb35f84e1d521ca
Darki255 <Kurachiy75@gmail.com>
2024-12-06 20:45:24 +0300

    Ключевое слово ref

---
+++ less_C#/Tasks/keyword/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace keyword;
+using System;
+
+class Program
+{
+    static int Summa(ref int first, ref int second)
+    {
+        return first + second;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первое число: ");
+        int a1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int a2 = int.Parse(Console.ReadLine());
+        
+        var result = Summa(ref a1, ref a2);
+        Console.WriteLine(result);
+    }
+}
