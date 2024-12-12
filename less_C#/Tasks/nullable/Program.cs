1cb5d1aa7d6118fe4752b3dc61fb5baa203c05ef
Darki255 <Kurachiy75@gmail.com>
2024-12-12 19:58:39 +0300

    nullable

---
+++ less_C#/Tasks/nullable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace nullable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        int a = null; // Не допускает присвоить NULL
+        System.Console.WriteLine(a;
+        */
+
+        int? a = null; // Допускает присвоить NULL
+        System.Console.WriteLine(a);
+
+        string? b = "6";
+        System.Console.WriteLine(b ?? "Нет данных"); // 6
+
+        string? с = null;
+        System.Console.WriteLine(с ?? "Нет данных"); // Нет данных
+    }
+}
