1c1b39a41d78f6fd159b6b966a4f447cab30242d
Darki255 <Kurachiy75@gmail.com>
2024-12-14 20:56:20 +0300

    enum

---
+++ less_C#/Tasks/enum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace enum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        enum Direction : byte
+        {
+            UpButton = 1,
+            DownButton = 2,
+            LeftButton = 3,
+            RightButton = 4
+        }
+            
+        var key = Console.ReadKey();
+
+        System.Console.WriteLine($"ID {(int)key}, кнопка {key}");
+    }
+}
