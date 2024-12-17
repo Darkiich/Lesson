7c9caa67b40237e685435ea7f8b7b030e987d1b5
Darki255 <Kurachiy75@gmail.com>
2024-12-17 22:15:03 +0300

    Конструктор класса

---
+++ less_C#/OOP/ConstructorClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConstructorClass;
+
+class Car
+{
+    // Конструктор класса начало
+    public Car(bool isRefilled)
+    {
+        _isRefilled = isRefilled;
+    }
+    // Конструктор класса конец
+
+    private bool _isRefilled = false;
+
+    private void Refilled()
+    {
+        System.Console.WriteLine("Идет заправка...");
+        _isRefilled = true;
+        System.Console.WriteLine("Заправлен!");
+    }
+
+    public void PowerEngine()
+    {
+        if (!_isRefilled)
+        {
+            System.Console.WriteLine("Нету топлива!");
+            Refilled();
+        }
+        System.Console.WriteLine("Двигатель работает");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car(false);
+        car.PowerEngine();
+    }
+}
