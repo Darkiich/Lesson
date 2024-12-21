5c3bec9d05a162e13a03d7a7d91c363a3941e586
Darki255 <Kurachiy75@gmail.com>
2024-12-21 20:37:11 +0300

    Статический конструктор

--- less_C#/OOP/AccessModifiers/obj/Debug/net8.0/AccessModifiers.AssemblyInfo.cs
+++ less_C#/OOP/AccessModifiers/obj/Debug/net8.0/AccessModifiers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessModifiers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d956308126e21a0b900ff8408b31558bcd486ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce13a89bba6377d59d6a43dd8572f0a038520b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessModifiers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessModifiers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
