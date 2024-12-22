13ed31ffe9fda562fbec1b9587b9cd6f2fbe50a1
bananchiki <87230164+Darkiich@users.noreply.github.com>
2024-12-22 17:14:29 +0000

    Метод расширения

---
+++ less_C#/OOP/ExtensionMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtensionMethod;
+
+static class MyExtensionMethod
+{
+    public static void Print(this DateTime dateTime)
+    {
+        Console.WriteLine($"Текущая дата: {dateTime}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime.Now.Print();
+    }
+}
