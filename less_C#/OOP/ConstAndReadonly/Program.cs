52344698e6f8c1dc2d53546a71e2729a225f74f9
bananchiki <87230164+Darkiich@users.noreply.github.com>
2024-12-23 19:12:28 +0000

    Ключевые слова const и readonly

---
+++ less_C#/OOP/ConstAndReadonly/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConstAndReadonly;
+
+class MyClass
+{
+    /*  Const - ключевое статическое слово, которое используется для объявления констант и не может быть изменено.
+        В константу должно быть присвоено какое-то значение до компиляции
+
+        Readonly - ключевое не статическое слово, которое может быть изменено в любой момент ТОЛЬКО В КОНСТРУКТОРЕ КЛАССА.
+        В readonly не обязательно присваивать значение до компиляции
+    */
+
+    public const int MY_CONST_VAR = 5;
+    public readonly int my_readonly_var_0;
+
+    public MyClass(int my_readonly_var_1)
+    {
+        my_readonly_var_0 = my_readonly_var_1;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(MY_CONST_VAR); // 5
+        Console.WriteLine(my_readonly_var_0); // 10
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myClass = new MyClass(10);
+        myClass.Print(); // 5, 10
+    }
+}
