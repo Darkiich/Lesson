22e2cf9a4e87ccb6c6a945cf1073df40fd53f915
bananchiki <87230164+Darkiich@users.noreply.github.com>
2024-12-25 19:42:38 +0000

    Наследование

---
+++ less_C#/OOP/Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Inheritance;
+
+class MyClass
+{
+    public string x { get; set; }
+    public string y { get; set; }
+
+    public void PrintX()
+    {
+        x ??= "0";
+        Console.WriteLine($"X: {x}");
+    }
+
+    public void PrintY()
+    {
+        y ??= "0";
+        Console.WriteLine($"Y: {y}");
+    }
+
+    public void PrintAll()
+    {
+        PrintX();
+        PrintY();
+    }
+}
+
+class Program : MyClass // Наследование предоставляет весь функционал наследуемого класса
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.x = "6";
+        program.y = "10";
+        program.PrintAll();
+    }
+}
