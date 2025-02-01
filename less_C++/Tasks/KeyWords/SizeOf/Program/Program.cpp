eaa7f06e1b950e29ce8035ba56a2269696f7d394
Darki255 <Kurachiy75@gmail.com>
2025-02-01 15:57:45 +0300

    Ключевое слово - sizeof

---
+++ less_C++/Tasks/KeyWords/SizeOf/Program/Program.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/* sizeof - это кл.слово, которое возвращает размер чего-то в байтах */
+
+	// Работает с инициализированными и неинициализированными
+	int a;
+	int b = 10;
+	cout << sizeof(a) << endl; // 4 байт
+	cout << sizeof(b) << endl; // 4 байт
+
+	/* sizeof(arr) / sizeof(arr[0]) - эта конструкция рассчитает кол-во элементов и пробежится по массиву */
+	int arr[]{1, 2, 3, 4, 5};
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		cout << arr[i];
+	}
+}
