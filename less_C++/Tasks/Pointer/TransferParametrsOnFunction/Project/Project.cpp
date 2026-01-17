72c56cdf58ac509aca3c0ae265d043838fa9ca85
Darkiich <ne>
2026-01-17 12:24:03 +0300

    передача указателя в функцию

---
+++ less_C++/Tasks/Pointer/TransferParametrsOnFunction/Project/Project.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void func(int* p)
+{
+	(*p)++; // Сначала обязательно разыменовываем указатель заключив в скобки(т.к высший приоритет), а затем делаем остальные действия, которые хотим
+}
+
+int main()
+{
+	int a = 5;
+	cout << "Before: " << a << endl;
+	func(&a);
+	cout << "After: " << a << endl;
+}
