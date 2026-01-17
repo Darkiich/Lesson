f893e7047966bcd56e8fd9c104181cfa551006cf
Darkiich <ne>
2026-01-17 14:03:03 +0300

    операторы new/delete

---
+++ less_C++/Tasks/DynamicMemory/new_and_delete/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// new - просит выделить память из оперативки. delete - освобождает память, выделенную с помощью new.
+
+int main()
+{
+	int* a = new int; // выделение памяти под целое число
+	*a = 10;
+	cout << *a << endl;
+	delete a; // освобождение памяти
+}
