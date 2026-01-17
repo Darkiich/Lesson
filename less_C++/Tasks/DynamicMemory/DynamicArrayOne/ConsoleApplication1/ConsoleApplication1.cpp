833f4916efe039d8ff26dc2478698cfabcb436cc
Darkiich <ne>
2026-01-17 14:57:21 +0300

    динамический массив

---
+++ less_C++/Tasks/DynamicMemory/DynamicArrayOne/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Динамический массив
+
+int main()
+{
+	srand(time(0));
+
+	int size;
+
+	cout << "Enter size: ";
+	cin >> size;
+
+	int* arr = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() & 10;
+	}
+
+	for (int i = 0; i < size; i++) // Если выйти за пределы этого массива, то там будет адресс памяти из оперативки
+	{
+		cout << *(arr + i) << " ";
+
+		if ((i + 1) % 4 == 0)
+		{
+			cout << endl;
+		}
+	}
+
+	delete[] arr; // Удаляем массив из выделенной памяти оперативки
+}
