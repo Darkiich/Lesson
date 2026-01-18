5d9236dfbeefb674ce1cf4f47a5340ef362fbc1c
Darkiich <ne>
2026-01-18 14:07:09 +0300

    константные параметры функции

---
+++ less_C++/Tasks/Function/ConstParametersFunctions/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Константный параметры нужны для того, чтобы защитить параметры от случайного изменения внутри функции.
+
+void fillArr(int* const arr, int const size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		*(arr + i) = (i + 1) * 2;
+	}
+}
+
+void showArr(int* const arr, int const size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(arr + i) << " ";
+	}
+}
+
+int main()
+{
+	int size = 10;
+	int* arr = new int[size];
+
+	fillArr(arr, size);
+	showArr(arr, size);
+}
