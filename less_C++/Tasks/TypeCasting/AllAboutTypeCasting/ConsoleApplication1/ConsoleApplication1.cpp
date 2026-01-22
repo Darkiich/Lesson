7a41a7a3b80a0f953e9988fc96c8d823a5bd92df
Darkiich <ne>
2026-01-23 00:01:33 +0300

    явно и неявное преобразование типов в стиле C

---
+++ less_C++/Tasks/TypeCasting/AllAboutTypeCasting/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Преобразование типов в стиле C
+int main()
+{
+	double pi = 3.14159;
+
+	// Явное преобразование типов
+	cout << (int)pi << endl; // Вывод: 3
+	cout << (bool)pi << endl; // Вывод: 1 (true). Если число не равно нулю, то результат true
+	cout << (char)pi << endl; // Вывод: символ ASCII
+}
