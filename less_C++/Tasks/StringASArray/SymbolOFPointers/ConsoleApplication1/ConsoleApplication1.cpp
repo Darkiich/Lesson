78a2772408bfc16f84ead265445581a985d6da66
Darkiich <ne>
2026-01-23 13:06:03 +0300

    строки и указатели

---
+++ less_C++/Tasks/StringASArray/SymbolOFPointers/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void strlenn(char* word)
+{
+	int counts = 0;
+	for (int i = 0; i < word[i] != '\0'; i++)
+	{
+		counts++;
+	}
+
+	cout << "Length: " << counts << endl;
+}
+
+void swapOnASCIISymbols(char* currentWordArr)
+{
+
+	for (int i = 0; i < currentWordArr[i] != '\0'; i++)
+	{
+		currentWordArr[i] = (char)i + 50;
+	}
+}
+
+
+int main()
+{
+	char str[] = "Hello, World!";
+	strlenn(str);
+
+	cout << "Before swap: " << str << endl;
+	swapOnASCIISymbols(str);
+	cout << "After swap: " << str << endl;
+}
