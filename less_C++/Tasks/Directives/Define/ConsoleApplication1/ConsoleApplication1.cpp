7ccc3ff9cebcb2cad7eea280eb830ceb4b9ca128
Darkiich <ne>
2026-01-23 17:44:45 +0300

    Директива define. define с параметрами

---
+++ less_C++/Tasks/Directives/Define/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#define MY_INT 5
+#define MY_INT_WITH_PARAMETERSS(a, b) (a + b)
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	cout << "Before: " << a << endl;
+
+	a = MY_INT;
+	cout << "After: " << a << endl;
+
+	cout << "Sum: " << MY_INT_WITH_PARAMETERSS(a, 10) << endl;
+}
