88d2c3bcc2c945f5d5055d776d7a9886e4127521
Darkiich <ne>
2026-01-24 13:15:06 +0300

    условная компиляция. Директивы if, ifdef, ifndef, elif, else

---
+++ less_C++/Tasks/Directives/ConditionalCompilation/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#define DEBUG
+//#define FUN
+#define LEVEL_DEBUG 5
+#define LEVEL_HAPPY 4
+
+using namespace std;
+
+// Условная компиляция
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+// Проверка наличия макросов. If defined(если определён)
+#ifdef DEBUG // Если макрос DEBUG определен
+	cout << "Дебаг определён." << endl;
+
+#else // Если макрос DEBUG не определен
+	cout << "Дебаг не определён." << endl;
+#endif
+
+
+
+
+// Проверка отсутствия макросов. If not defined(если не определён)
+#ifndef FUN // Если макрос FUN не определен
+	cout << "Фан не определён." << endl;
+
+#else // Если макрос FUN определен
+	cout << "Фан определён." << endl;
+#endif
+
+
+
+
+// Проверка на значение макроса. If (если)
+#if LEVEL_DEBUG > 4 // Если значение макроса LEVEL_DEBUG больше 4
+	cout << "Уровень дебага больше 4." << endl;
+
+#else // Если значение макроса LEVEL_DEBUG не больше 4
+	cout << "Уровень дебага не больше 4." << endl;
+#endif
+
+
+
+
+// Проверка на значение макроса. If (если) и elif (иначе если)
+#if LEVEL_HAPPY < 4 // Если значение макроса LEVEL_DEBUG равно 4
+	cout << "Уровень дебага больше 4." << endl;
+
+#elif LEVEL_HAPPY > 0 // Если значение макроса LEVEL_DEBUG больше 0
+	cout << "Уровень дебага больше 0." << endl;
+
+#else // Во всех остальных случаях
+	cout << "Уровень дебага равен 0." << endl;
+#endif
+}
