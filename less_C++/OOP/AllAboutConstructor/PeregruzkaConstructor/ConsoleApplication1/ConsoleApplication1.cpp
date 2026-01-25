9db0c1546e9c563474b54794838457ef381869aa
Darkiich <ne>
2026-01-25 13:05:37 +0300

    перегрузка конструктора

---
+++ less_C++/OOP/AllAboutConstructor/PeregruzkaConstructor/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Point {
+private:
+	int x, y;
+
+public:
+	Point() {
+		x = 0;
+		y = 0;
+	}
+
+	Point(int s_x, int s_y) {
+		x = s_x;
+		y = s_y;
+	}
+
+	void display() {
+		cout << "X: " << x << " Y: " << y << endl;
+	}
+};
+
+int main()
+{
+	Point p1;
+	Point p2(10, 20);
+
+	p1.display();
+	p2.display();
+}
