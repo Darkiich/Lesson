f2a20f211b666f670a47e99bb0d7a00919db3da3
Darkiich <ne>
2026-01-25 13:34:08 +0300

    деструктор

---
+++ less_C++/OOP/AllAboutConstructor/Destructor/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class MyClass {
+private:
+	int* arr;
+public:
+	// Конструктор вызывается при создании объекта класса
+	MyClass(int size) {
+		cout << "Constructor is calling" << endl;
+		arr = new int[size];
+
+		for (int i = 0; i << size; i++) {
+			*(arr + i) = i;
+		}
+	}
+
+	// Деструктор вызывается тогда, когда экзепляр класса перестаёт быть в области видимости функции и.т.д. То есть при выходе из функции вызовется деструктор
+	~MyClass() {
+		cout << "Destructor is calling" << endl;
+		delete[] arr;
+	}
+};
+
+void PrintArr(int size)
+{
+	cout << "Start PrintArr" << endl;
+	MyClass myClass(size);
+	cout << "End PrintArr" << endl;
+}
+
+int main()
+{
+	PrintArr(10);
+
+	return 0;
+}
--- less_C++/OOP/AllAboutConstructor/PeregruzkaConstructor/ConsoleApplication1/ConsoleApplication1.cpp
+++ less_C++/OOP/AllAboutConstructor/PeregruzkaConstructor/ConsoleApplication1/ConsoleApplication1.cpp
@@ -29,4 +29,6 @@ int main()
 
 	p1.display();
 	p2.display();
+
+	return 0;
 }
