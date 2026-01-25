60ba4f7f056b3e1a131633869b47c4f13cd5359d
Darkiich <ne>
2026-01-25 12:46:30 +0300

    геттеры и сеттеры

---
+++ less_C++/OOP/GetterANDSetter/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Point {
+private:
+		int x;
+		int y;
+public:
+	// Геттеры и сеттеры
+	int getterX() { return x; }
+	int getterY() { return y; }
+
+	void setterX(int const valueX) { x = valueX; }
+	void setterY(int const valueY) { y = valueY; }
+};
+
+int main()
+{
+	Point point;
+	point.setterX(10);
+	point.setterY(20);
+	cout << "X: " << point.getterX() << ", Y: " << point.getterY() << endl;
+}
