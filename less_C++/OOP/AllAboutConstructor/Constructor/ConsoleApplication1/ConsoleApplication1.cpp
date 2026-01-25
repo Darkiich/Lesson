8de3354b4864666607507276bc26b566ccccce66
Darkiich <ne>
2026-01-25 13:02:08 +0300

    конструктор класса

---
+++ less_C++/OOP/AllAboutConstructor/Constructor/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Point {
+private:
+	int x, y;
+public:
+	Point(int s_x, int s_y) {
+		x = s_x;
+		y = s_y;
+	}
+
+	void display() {
+		cout << "Point(" << x << ", " << y << ")" << endl;
+	}
+};
+
+int main()
+{
+	Point p(10, 20);
+	p.display();
+}
