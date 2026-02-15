c3d0d9411eeb9f7de9c3e042bc0d992046d92e66
Darkiich <ne>
2026-02-15 14:30:15 +0300

    дружественная функция класса

---
+++ less_C++/OOP/friendFuncOfClass/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class TV;
+
+class Human {
+public:
+    bool checkIsPoweredTV(TV& tv);
+};
+
+class TV {
+private:
+    int channel;
+    int volumeLevel;
+	bool power;
+public:
+    TV() {
+        channel = 1;
+        volumeLevel = 1;
+		power = false;
+	}
+
+    TV(int channel, int volumeLevel, bool power) {
+        this->channel = channel;
+        this->volumeLevel = volumeLevel;
+        this->power = power;
+	}
+
+	friend bool Human::checkIsPoweredTV(TV& tv);
+};
+
+bool Human::checkIsPoweredTV(TV& tv) {
+    tv.power == false ? cout << "TV is off" : cout << "TV is on";
+    return tv.power;
+}
+
+int main()
+{
+    TV tv1(5, 10, true);
+    Human human1;
+	human1.checkIsPoweredTV(tv1);
+}
