755274623e2c7c0bf196e12209b2d02f4e556a8b
Darkiich <ne>
2026-05-29 14:43:55 +0300

    Добавил информацию к переменным про sizeof и size_t

--- less_C/AllAboutVariable/main.c
+++ less_C/AllAboutVariable/main.c
@@ -40,6 +40,17 @@ void main()
 	printf("long double: %.18Lf\n\n", ldnumber);
 
 	// ============== Размеры типов ==============
+
+	// sizeof - оператор, который вернёт размер в байтах указанного операнда
+	// sizeof - также возвращает размер в виде значения size_t
+	// size_t - представляет псевдоним для unsigned int. На других системах может быть другим
+
+	size_t char_size_t = sizeof(char);
+	size_t float_size_t = sizeof(float);
+	size_t long_size_t = sizeof(long);
+	size_t short_size_t = sizeof(short);
+	printf("SIZE_T TYPE VARIABLE: \nsize_t char_size_t = %d\nsize_t float_size_t = %d\nsize_t long_size_t = %d\nsize_t short_size_t = %d\n\n", char_size_t, float_size_t, long_size_t, short_size_t);
+
 	printf("SIZES OF TYPES (in bytes):\n");
 	printf("char: %zu\n", sizeof(char));
 	printf("short: %zu\n", sizeof(short));
